217617a4f5ba33327c8e77ee8d49da21bcbb53e7
Yashwant Singh <Yashwant.Singh@amd.com>
2023-07-15 17:42:58 +0530

    Basic dot-graph-support

--- llvm/lib/TableGen/DotRecordsBackend.cpp
+++ llvm/lib/TableGen/DotRecordsBackend.cpp
@@ -10,11 +10,16 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <unordered_map>
 #include "llvm/ADT/DenseMapInfo.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/TableGen/TableGenBackend.h"
+#include "llvm/TableGen/Record.h"
 
 #define DEBUG_TYPE "detailed-records-backend"
+
+#define NL "\n"
 
 namespace llvm {
 class RecordKeeper;
@@ -25,21 +30,92 @@ using namespace llvm;
 
 namespace {
 
+using NodeMap = std::unordered_map<std::string, int>;
 // Any helper data structures can be defined here. Some backends use
 // structs to collect information from the records.
 
 class DotRecordsEmitter {
 private:
   RecordKeeper &Records;
-
+  NodeMap Nodes;
 public:
   DotRecordsEmitter(RecordKeeper &RK) : Records(RK) {}
 
+  void printEdges(raw_ostream &OS);
+  void printNodes(raw_ostream &OS);
+  void indexAllNodes();
+  void generateDotGraph(raw_ostream &OS);
   void run(raw_ostream &OS);
   void printHello(raw_ostream &OS);
 }; // emitter class
 
 } // anonymous namespace
+
+void DotRecordsEmitter::printEdges(raw_ostream &OS) {
+  const auto &ClassList = Records.getClasses();
+  for(const auto &ClassPair: ClassList){
+    auto *const Class = ClassPair.second.get();
+    auto Superclasses = Class->getSuperClasses();
+    std::string ChildLabel = Class->getNameInitAsString();
+    std::string ParentLabel;
+    int ChildIndex = Nodes[ChildLabel];
+    int ParentIndex;
+    for (const auto &SuperclassPair : Superclasses) {
+      auto *SuperClass = SuperclassPair.first;
+      ParentLabel = SuperClass->getNameInitAsString();
+      assert(Nodes.find(ParentLabel) != Nodes.end() && "Parent node not indexed");
+      ParentIndex = Nodes[ParentLabel];
+      // "node0" -> "node4";
+      OS << "\"" << ChildIndex << "\" -> \"" << ParentIndex << "\";" << NL;
+    }     
+  }
+}
+
+void DotRecordsEmitter::printNodes(raw_ostream &OS) {
+  for(auto NodeInfo: Nodes){
+    std::string NodeLabel = NodeInfo.first;
+    int NodeIndex = NodeInfo.second;
+    OS << NodeIndex << "[label = \"" << NodeLabel << "\"];" << NL;
+  }
+}
+
+void DotRecordsEmitter::indexAllNodes(){
+  const auto &ClassList = Records.getClasses();
+  int Index = 0;
+  for(const auto &ClassPair: ClassList){
+    auto *const Class = ClassPair.second.get();
+    Nodes[Class->getNameInitAsString()] = Index++;
+  }
+}
+
+void DotRecordsEmitter::generateDotGraph(raw_ostream &OS) {
+  // TODO: Expand the graph to cover nodes outside the current file
+  OS << "digraph g {" << NL;
+  
+  // Node type info
+  OS << "node [shape = box];" << NL;
+
+  // Generate Nodes
+  printNodes(OS);
+
+  // Generate Edges
+  printEdges(OS);
+
+  OS << "}" << NL;
+}
+
+void DotRecordsEmitter::run(raw_ostream &OS) {
+  // printHello(OS);
+
+  // No need to print file header in .dot graph
+  // emitSourceFileHeader("Skeleton data structures", OS);
+
+  indexAllNodes();
+  generateDotGraph(OS);
+
+  (void)Records; // To suppress unused variable warning; remove on use.
+}
+
 
 // Print the report heading, including the source file name.
 void DotRecordsEmitter::printHello(raw_ostream &OS) {
@@ -48,16 +124,6 @@ void DotRecordsEmitter::printHello(raw_ostream &OS) {
   OS << "Debug string is 'detailed-records-backend'\n";
   OS << "Invoke using: llvm-tablegen -print-dot-records input.td \n";
 }
-
-void DotRecordsEmitter::run(raw_ostream &OS) {
-
-  printHello(OS);
-
-  emitSourceFileHeader("Skeleton data structures", OS);
-
-  (void)Records; // To suppress unused variable warning; remove on use.
-}
-
 
 namespace llvm {
 
