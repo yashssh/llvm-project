5013c74f94fb93c5dc01cacacd0040c6437d033e
Yashwant Singh <Yashwant.Singh@amd.com>
2023-07-16 23:57:15 +0530

    Cleanup

--- llvm/lib/TableGen/DotRecordsBackend.cpp
+++ llvm/lib/TableGen/DotRecordsBackend.cpp
@@ -6,26 +6,26 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This Tablegen backend emits ...
+// This Tablegen backend emits the inheritance hierarchy of a tablegen record
+// or a class as a dot graph, where each node represents a class/record with 
+// additional details such as template args and member variables.
 //
 //===----------------------------------------------------------------------===//
 
-#include <unordered_map>
-#include <queue>
 #include "llvm/ADT/DenseMapInfo.h"
-#include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/StringRef.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/GraphWriter.h"
-#include "llvm/TableGen/TableGenBackend.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/TableGen/Record.h"
+#include "llvm/TableGen/TableGenBackend.h"
+#include <queue>
+#include <unordered_map>
 
 #define DEBUG_TYPE "detailed-records-backend"
 
 #define NL "\n"
-
-
 namespace llvm {
 class RecordKeeper;
 class raw_ostream;
@@ -36,24 +36,25 @@ using namespace llvm;
 namespace {
 
 // TODO: Can make this parameter hidden
-// TODO: Find a way to accomodate records too and not just classes
+// TODO: Use single option instead of 2
+// TODO: Support multiclass and defm
 static cl::opt<std::string>
-FilterClass("filter-class", cl::desc("Class to generate dot graph for"), cl::value_desc("ClassName"), cl::init(""));
+    FilterClass("filter-class", cl::desc("Class to generate dot graph for"),
+                cl::value_desc("ClassName"), cl::init(""));
 
-static cl::opt<std::string>
-FilterDef("filter-def", cl::desc("Def to generate dot graph for"), cl::value_desc("DefName"), cl::init(""));
+static cl::opt<std::string> FilterDef("filter-def",
+                                      cl::desc("Def to generate dot graph for"),
+                                      cl::value_desc("DefName"), cl::init(""));
 
-using NodeMap = std::unordered_map<Record*, int>;
-// Any helper data structures can be defined here. Some backends use
-// structs to collect information from the records.
+using NodeMap = std::unordered_map<Record *, int>;
 
 class DotRecordsEmitter {
 private:
   RecordKeeper &Records;
   NodeMap Nodes;
 
-  void printGraphEdges(raw_ostream &OS, Record& RootNode);
-  void printNodeLabel(Record* Node, raw_ostream &OS);
+  void printGraphEdges(raw_ostream &OS, Record &RootNode);
+  void printNode(Record *Node, raw_ostream &OS);
   void printNodes(raw_ostream &OS);
   void indexGraphNodes(Record &RootNode);
   void generateDotGraph(raw_ostream &OS, Record &RootNode);
@@ -65,128 +66,126 @@ public:
 
 } // anonymous namespace
 
-static void printEdge(Record& ChildNode, Record& ParentNode, NodeMap& Nodes, raw_ostream &OS) {
-    std::string ChildLabel = ChildNode.getNameInitAsString();
-    std::string ParentLabel = ParentNode.getNameInitAsString();
-    int ChildIndex = Nodes[&ChildNode];
-    int ParentIndex = Nodes[&ParentNode];
-    assert(Nodes.find(&ChildNode) != Nodes.end() && "Child node not indexed");
-    assert(Nodes.find(&ParentNode) != Nodes.end() && "Parent node not indexed");
-    OS << "\"" << ChildIndex << "\" -> \"" << ParentIndex << "\";" << NL;
+static void printEdge(Record &ChildNode, Record &ParentNode, NodeMap &Nodes,
+                      raw_ostream &OS) {
+  int ChildIndex = Nodes[&ChildNode];
+  int ParentIndex = Nodes[&ParentNode];
+  assert(Nodes.find(&ChildNode) != Nodes.end() && "Child node not indexed");
+  assert(Nodes.find(&ParentNode) != Nodes.end() && "Parent node not indexed");
+  OS << "\"" << ChildIndex << "\" -> \"" << ParentIndex << "\";" << NL;
 }
 
 void DotRecordsEmitter::printGraphEdges(raw_ostream &OS, Record &RootNode) {
-  std::queue<Record*>Queue;
+  std::queue<Record *> Queue;
   Queue.push(&RootNode);
-  while(Queue.size()){
-    Record* Child = Queue.front();
+  while (Queue.size()) {
+    Record *Child = Queue.front();
     Queue.pop();
 
     // Print child to parent edge and push parents to queue
-    SmallVector<Record*>ParentClasses;
+    SmallVector<Record *> ParentClasses;
     Child->getDirectSuperClasses(ParentClasses);
-    // OS << "Creating edges from " << Child->getNameInitAsString() << " to " << NL;
-    for (Record* Parent : ParentClasses){
+    for (Record *Parent : ParentClasses) {
       printEdge(*Child, *Parent, Nodes, OS);
       Queue.push(Parent);
-      // OS << Parent->getNameInitAsString() << NL;
     }
-    // OS << NL;
   }
+  OS << NL;
 }
 
-void DotRecordsEmitter::printNodeLabel(Record* Node, raw_ostream &OS){
+// TODO: Can use formatv for better string formatting
+void DotRecordsEmitter::printNode(Record *Node, raw_ostream &OS) {
   std::string NodeName = Node->getNameInitAsString();
   int NodeIndex = Nodes[Node];
-  if (!Node->isClass()){
-    OS << std::to_string(NodeIndex) +  "[shape=ellipse label=\"" + NodeName + "\" ];\n";
+  if (!Node->isClass()) {
+    OS << std::to_string(NodeIndex) + "[shape=ellipse label=\"" + NodeName +
+              "\" ];\n";
     return;
   }
 
-  // TODO: Can use formatv for better string formatting
-  OS << std::to_string(NodeIndex) +  "[shape=record label=\"" + NodeName + " | {Template args | ";
+  OS << std::to_string(NodeIndex) + "[shape=record label=\"" + NodeName +
+            " | {Template args | ";
   ArrayRef<Init *> TemplateArgs = Node->getTemplateArgs();
   for (const Init *TA : TemplateArgs) {
     const RecordVal *Value = Node->getValue(TA);
-    // FIXME: Tablegen string escape issue in generated string hence can't use OS << *Value;
-    OS << DOT::EscapeString(Value->getPrintType()) + " " + Value->getNameInitAsString() + "\\n";
+    // FIXME: Tablegen string escape issue in generated string hence can't use
+    // OS << *Value;
+    OS << DOT::EscapeString(Value->getPrintType()) + " " +
+              Value->getNameInitAsString() + "\\n";
   }
   OS << "}\" ];\n";
+  // Member variables
+  ArrayRef<RecordVal>Values = Node->getValues(); 
+  for(auto Val: Values){
+    OS << "// "; 
+    if(Val.isTemplateArg())
+      OS << "Template arg: ";
+    else if(Val.isNonconcreteOK())
+      OS << "Non Concrete arg: ";
+    else 
+      OS << "Normal arg: ";
+    OS << Val;
+  }
+  OS << NL;
 }
 
 void DotRecordsEmitter::printNodes(raw_ostream &OS) {
-  for(auto NodeInfo: Nodes){
-    Record* Node = NodeInfo.first;
-    printNodeLabel(Node, OS);
-    OS << NL;
+  for (auto NodeInfo : Nodes) {
+    Record *Node = NodeInfo.first;
+    printNode(Node, OS);
   }
 }
 
 void DotRecordsEmitter::indexGraphNodes(Record &RootNode) {
   int index = 0;
-  std::queue<Record*>Queue;
-  // SmallVectorImpl<Record *> Classes;
+  std::queue<Record *> Queue;
   Queue.push(&RootNode);
-  while(Queue.size()){
-    Record* Current = Queue.front();
-    std::string ClassName = Current->getNameInitAsString();
+  while (Queue.size()) {
+    Record *Current = Queue.front();
     Queue.pop();
-    
+
     // Skip visited nodes
-    if(Nodes.find(Current) != Nodes.end())
+    if (Nodes.find(Current) != Nodes.end())
       continue;
 
     Nodes[Current] = index++;
-    
+
     // Push parent nodes into queue
-    // FIXME: Use getDirectSuperClasses
-    auto Superclasses = Current->getSuperClasses();
-    for (const auto &SuperClassPair : Superclasses)
-      Queue.push(SuperClassPair.first);
-    
+    SmallVector<Record *> ParentClasses;
+    Current->getDirectSuperClasses(ParentClasses);
+    for (const auto ParentClass : ParentClasses)
+      Queue.push(ParentClass);
   }
 }
 
 void DotRecordsEmitter::generateDotGraph(raw_ostream &OS, Record &RootNode) {
-  // TODO: Expand the graph to cover nodes outside the current file
-  OS << "digraph g {" << NL;
-  
-  // Node type info
-  OS << "node [shape = box];" << NL;
-
+  OS << "// Root Node is " << RootNode.getNameInitAsString() << NL;
+  OS << "digraph RecordDependencyGraph {" << NL;
   // Generate Nodes
   printNodes(OS);
-
   // Generate Edges
   printGraphEdges(OS, RootNode);
-
   OS << "}" << NL;
 }
 
 void DotRecordsEmitter::run(raw_ostream &OS) {
-  if(FilterClass.empty() && FilterDef.empty()){
+  if (FilterClass.empty() && FilterDef.empty()) {
     OS << "Specify either -filter-class or -filter-def" << NL;
     return;
   }
-  if(!FilterClass.empty() && !FilterDef.empty()){
+  if (!FilterClass.empty() && !FilterDef.empty()) {
     OS << "Specify only one out of -filter-class and -filter-def" << NL;
     return;
   }
-  Record* RootNode;
-  // FIXME: Add asserts for invalid Class or Def name
-  if(!FilterClass.empty())
+  Record *RootNode;
+  // TODO: Add asserts for invalid Class or Def name
+  if (!FilterClass.empty())
     RootNode = Records.getClass(FilterClass);
   else
     RootNode = Records.getDef(FilterDef);
-  OS << "Root Node is " << RootNode->getNameInitAsString() << NL;
-
-  // FIXME: No need to print file header in .dot graph, will cause parsing error
-  // emitSourceFileHeader("Skeleton data structures", OS);
 
   indexGraphNodes(*RootNode);
   generateDotGraph(OS, *RootNode);
-
-  (void)Records; // To suppress unused variable warning; remove on use.
 }
 
 namespace llvm {
